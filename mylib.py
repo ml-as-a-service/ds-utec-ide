b5cadb3f8a605b0990775a3510cf6807cdbd79c9
Mario Garcia <mariogarcia.ar@gmail.com>
2023-07-16 13:58:13 -0300

    fix chromium version

--- mylib.py
+++ mylib.py
@@ -7,6 +7,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support import expected_conditions as EC
 
+from webdriver_manager.chrome import ChromeDriverManager
+
 import time 
 import csv 
 import os 
@@ -49,7 +51,8 @@ def getDriver(url):
     chromeOptions.add_experimental_option("prefs",prefs)
     chromedriver = "chromedriver"
 
-    driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=chromeOptions)
+    driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chromeOptions)
+    # driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=chromeOptions)
 
     # Open the main page
     driver.get(url)    
